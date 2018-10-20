a5210264d664de714eb87df9dd3d2f90e4c660f9
Aravind Patnam <akpatnam@shyams-imac.T-mobile.com>
2018-10-20 13:02:21 -0700

    Basic loginUI - start working off of this with new branches

--- Nutrivis/app/src/main/java/com/example/srini/nutrivisai/MainActivity.java
+++ Nutrivis/app/src/main/java/com/example/srini/nutrivisai/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.srini.nutrivisai;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +14,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SearchView searchView = findViewById(R.id.searchView);
-        String suggestWord = "Search for Scanned Items";
-        searchView.setQueryHint(suggestWord);
-        searchView.clearFocus();
 
-        ListView listView = findViewById(R.id.listView);
-        List<String> listItems = new ArrayList<>();
-        listItems.add("No Items Scanned");
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 
-        listView.setAdapter(adapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

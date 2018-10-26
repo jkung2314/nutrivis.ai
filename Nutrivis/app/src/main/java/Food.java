3d8757a4decf12fed4f5f9e101fdccda2bb1171a
Srinivasan Rangarajan <srini6773@gmail.com>
2018-10-26 16:39:20 -0700

    food class full functionality

--- Nutrivis/app/src/main/java/Food.java
+++ Nutrivis/app/src/main/java/Food.java
@@ -1,3 +1,9 @@
+
+
+import android.annotation.SuppressLint;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Food {
@@ -5,17 +11,21 @@ public class Food {
     private String name;
     private double fatContent;
     private int calorieCount;
-    private Date dateScanned;
+    private String dateScanned;
 
-    public Food(String foodName, double fatContent, int calorieCount, Date dateScanned){
+    @SuppressLint("NewApi")
+    public Food(String foodName, double fatContent, int calorieCount){
         name = foodName;
         this.fatContent = fatContent;
         this.calorieCount = calorieCount;
-        this.dateScanned = dateScanned;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        this.dateScanned = dtf.format(now);
     }
 
     public String toString(){
-        return name + "Fat: "+ fatContent + "g, Calories: " + calorieCount + " Scanned on: " + dateScanned;
+        return name + ": Fat: "+ fatContent + "g Calories: " + calorieCount + " Scanned on: " + dateScanned;
     }
 
     public String getName(){
@@ -30,7 +40,8 @@ public class Food {
         return calorieCount;
     }
 
-    public Date getDateScanned(){
+    public String getDateScanned(){
         return dateScanned;
     }
 }
+

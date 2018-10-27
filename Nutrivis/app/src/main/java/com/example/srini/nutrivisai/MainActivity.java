ff64262f374f4e5b24265fdca99b339f23ccff40
Srinivasan Rangarajan <srini6773@gmail.com>
2018-10-26 17:14:16 -0700

    authui removed from mainactivity

--- Nutrivis/app/src/main/java/com/example/srini/nutrivisai/MainActivity.java
+++ Nutrivis/app/src/main/java/com/example/srini/nutrivisai/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        //startActivity(intent);
 
         ListView listView = findViewById(R.id.lv);
 

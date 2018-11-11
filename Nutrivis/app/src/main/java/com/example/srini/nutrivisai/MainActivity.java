df3e1069b299c536afc567afbb20150d36697899
Hooshing Schaefer <hschaefe@ucsc.edu>
2018-11-10 21:10:26 -0800

    signout added

--- Nutrivis/app/src/main/java/com/example/srini/nutrivisai/MainActivity.java
+++ Nutrivis/app/src/main/java/com/example/srini/nutrivisai/MainActivity.java
@@ -115,17 +115,20 @@ public class MainActivity extends AppCompatActivity implements AsyncRequester{
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                mFirebaseAuth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
+
+
+
 }

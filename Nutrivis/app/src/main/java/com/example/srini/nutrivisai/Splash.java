d31c9da46bb9453b5689a05c34253fa3d93320a7
dan-rds <ddrichar@ucsc.edu>
2018-11-29 17:35:06 -0800

    food testing

--- Nutrivis/app/src/main/java/com/example/srini/nutrivisai/Splash.java
+++ Nutrivis/app/src/main/java/com/example/srini/nutrivisai/Splash.java
@@ -72,14 +72,14 @@ public class Splash extends AppCompatActivity {
     public void getUserData(FirebaseUser user) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-            final DocumentReference docRef = db.collection("userData").document(user.getUid()+".foods");
+            final DocumentReference docRef = db.collection("userData").document(user.getUid());
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                     if (task.isSuccessful()) {
                         DocumentSnapshot document = task.getResult();
                         if (document.exists()) {
-                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            Log.d(TAG, "DocumentSnapshot data: " + document.get("foods"));
                             triggerMain( document.getData().toString());
 
                         } else {
